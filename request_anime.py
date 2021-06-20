20cd7ac55aab5dbe5966ac695362b6eeed962090
JM-Yacho <jmyach08@gmail.com>
2021-06-20 18:18:23 -0500

    Switched from the official MAL api, to Jikan MAL api. Api requests for anime by genre work now

--- request_anime.py
+++ request_anime.py
@@ -1,163 +1,122 @@
-import os
-import time
 import json
 import requests
-import secrets
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from random import randrange
+import urllib.request
 
-cred_file = open("./text_files/credentials.json", "r")
-CREDENTIALS = json.loads(cred_file.read())
-cred_file.close()
+genres = {
+    'action': 1,
+    'adventure': 2,
+    'cars': 3,
+    'comedy': 4,
+    'dementia': 5,
+    'demons': 6,	
+    'mystery': 7,
+    'drama': 8,
+    'ecchi': 9,	
+    'fantasy': 10,	
+    'game': 11,	
+    'hentai': 12,	
+    'historical': 13,
+    'horror': 14,	
+    'kids': 15,	
+    'magic': 16,	
+    'martial arts': 17,
+    'mecha': 18,	
+    'music': 19,	
+    'parody': 20,	
+    'samurai': 21,	
+    'romance': 22,	
+    'school': 23,	
+    'sci fi': 24,	 
+    'shoujo': 25,	
+    'shoujo ai': 26,
+    'shounen': 27,	
+    'shounen ai': 28,
+    'space': 29,
+    'sports': 30,	
+    'super power': 31,
+    'vampire': 32,	
+    'yaoi': 33,	
+    'yuri': 34,	
+    'harem': 35,	
+    'slice of life': 36,
+    'supernatural': 37,
+    'military': 38,
+    'police': 39,		
+    'psychological': 40,
+    'thriller': 41,
+    'seinen': 42,	
+    'josei': 43
+}
 
-CLIENT_ID = CREDENTIALS['CLIENT_ID']
-EMAIL = CREDENTIALS['EMAIL']
-PASSWORD = CREDENTIALS['PASSWORD']
-AUTH_CODE = ""
-CODE_CHALLENGE = ""
 
-def get_new_code_challenge_and_state():
-    challenge = secrets.token_urlsafe(100)
-    state = secrets.token_urlsafe(20)
-    return challenge[:128], state[:24]
-
-def get_new_auth_code():
-    global CODE_CHALLENGE, CLIENT_ID, EMAIL, PASSWORD
-
-    # Create authorize url
-    CODE_CHALLENGE, state = get_new_code_challenge_and_state()
-    base_url = "https://myanimelist.net/v1/oauth2/authorize"
-    response_type = "code"
-    url = f'{base_url}?response_type={response_type}&client_id={CLIENT_ID}&code_challenge={CODE_CHALLENGE}&state={state}'
-
-    # Open url in chrome
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get(url)
-
-    # Wait for page to load
-    driver.implicitly_wait(5)
-
-    # Choose to login with google
-    loginWithGoogle = driver.find_element_by_xpath('//*[@id="content"]/div/section[1]/div/div[1]/a[1]')
-    loginWithGoogle.click()
-
-    # Input email
-    emailBox = driver.find_element_by_xpath('//*[@id ="identifierId"]')
-    emailBox.send_keys(EMAIL)
-
-    # Hit next
-    nextButton = driver.find_elements_by_xpath('//*[@id ="identifierNext"]')
-    nextButton[0].click()
-  
-    # Input password
-    passWordBox = driver.find_element_by_xpath('//*[@id ="password"]/div[1]/div / div[1]/input')
-    passWordBox.send_keys(PASSWORD)
-  
-    # Hit next
-    nextButton = driver.find_elements_by_xpath('//*[@id ="passwordNext"]')
-    nextButton[0].click()
-
-    # Wait for page to load
-    driver.implicitly_wait(5)
-
-    # Hit allow button
-    # This authoirizes this client to access the MAL account
-    allowButton = driver.find_elements_by_xpath('//*[@id="content"]/div/div/div/form/input[2]')
-    allowButton[0].click()
-    
-    # Wait for page to connect
-    time.sleep(7)
-
-    # Capture the redirect url
-    final_url = driver.current_url
-    # Exit the browser
-    driver.quit()
-
-    # Select the code and state parameters from the redirect url
-    returned_parameters = final_url.split("code=")[1]
-    returned_code, returned_state = returned_parameters.split("&state=")
-
-    # If the returned state does NOT equal the sent state
-    # There's an issue return a blank code
-    if state != returned_state:
-        returned_code = ""
-
-    return returned_code
-    
-def generate_new_tokens(grant_type, grant_value):
-    # grant_types: 'refresh_token', 'code'
-    global CLIENT_ID, CODE_CHALLENGE, AUTH_CODE
-
-    url = 'https://myanimelist.net/v1/oauth2/token'
-    data = {
-        'client_id': CLIENT_ID,
-        'grant_type': grant_type
+def get_anime_with_genre(requested_genre):
+    final_result = {
+        'successful': True,
     }
 
-    if grant_type == 'authorization_code':
-        # If the authorization code is blank
-        # Try to generate a new one
-        if grant_value == '':
-            AUTH_CODE = get_new_auth_code()
+    # Format the requested genre
+    requested_genre = (requested_genre.lower()).strip()
 
-        data['code_verifier'] = CODE_CHALLENGE
-        data['code'] = AUTH_CODE
+    # Try to save the id of the requested genre
+    try: 
+        genre_id = genres[requested_genre]
+    except KeyError:
+        # Error Logging
+        print(f'Error in get_anime_with_genre(): requested genre "{requested_genre}" does not exist')
+        if final_result['successful']: final_result['successful'] = False
+        return final_result
+
+    # Request the top {limit} anime currently airing with the requested genre
+    media_type = 'anime'
+    media_status = 'airing'
+    limit = 10
+    url = f'https://api.jikan.moe/v3/search/{media_type}?page=1&status={media_status}&genre={genre_id}&limit={limit}&order_by=score'
+    response = requests.get(url)
+    response_data = response.json()
+
+    # If the request was NOT successful
+    if response.status_code != 200:
+        # Error Logging
+        print(f'Error in get_anime_with_genre(): received status code {response.status_code} from {url}')
+        print(f'Response from api: {response_data}')
+        if final_result['successful']: final_result['successful'] = False
     else:
-        data[grant_type] = grant_value
+        # Index of the returned anime we will select
+        anime_index = randrange(limit)
+        # Json array of all animes returned
+        returned_anime = response_data['results']
 
-    response = requests.post(url, data)
-    response.close()
-
-    # If the token request was successful
-    if response.status_code == 200:
-        # Write the response to 'mal_tokens.json'
-        with open('./text_files/mal_tokens.json', 'w') as file:
-            json.dump(response.json(), file, indent = 4)
-            print('Tokens saved in "mal_tokens.json"')
-
-        return "Success"
-    else:
-        print(f'error {response.status_code}:{response.json()} while retrieving from {url} with {data}')
-        return ""
-
-def retrieve_token(token_type):
-    global AUTH_CODE
-
-    try:
-        f = open('./text_files/mal_tokens.json',)
-    # If 'mal_tokens.json' fails to open
-    except IOError:
-        # Try to generate new tokens
-        if generate_new_tokens('authorization_code', AUTH_CODE) != "":
-            f = open('./text_files/mal_tokens.json',)           
+        # If no anime were returned
+        if len(returned_anime) == 0:
+            # Error logging
+            print(f'Error in get_anime_with_genre(): no anime returned from {url}')
+            if final_result['successful']: final_result['successful'] = False
+        # Else, 1+ anime was/were returned
         else:
-            return ""
-    
-    token_data = json.load(f)
-    result = token_data[token_type]
-    f.close()
-    return result
+            selected_anime = returned_anime[anime_index]
+            # Path for jpg to store the anime cover photo
+            image_file_path = f'./images/{selected_anime["mal_id"]}.jpg'
 
-def get_anime_with_genre(genre):
-    url = f'https://api.myanimelist.net/v2/anime/ranking?ranking_type=airing&limit=10&genre=comedy'
-    response = requests.get(url, headers={'Authorization': f'Bearer {retrieve_token("access_token")}'})
-    
-    # If the request is unathorized
-    # Try to generate new tokens
-    if response.status_code == 401:
-        response.close()
-        generate_new_tokens('refresh_token', retrieve_token('refresh_token'))
-        response = requests.get(url, headers={'Authorization': f'Bearer {retrieve_token("access_token")}'})
-    
-    # If the request is successful
-    if response.status_code == 200:
-        result = response.json()
-    else:
-        print(f'error {response.status_code}:{response.json()} while retrieving from {url}')
-        result = ""
+            try:
+                # Open the image file, and save the jpg cover photo of the selected anime
+                image_file = open(image_file_path, 'w')
+                urllib.request.urlretrieve(selected_anime['image_url'], image_file_path)
+                image_file.close()
 
-    response.close()
-    print(result)
-    return result
-
-get_anime_with_genre('comedy')
+                # Add the selected anime's info to the final result
+                final_result['title'] = selected_anime['title']
+                final_result['synopsis'] =  selected_anime['synopsis']
+                final_result['image_file_path'] = image_file_path
+            except IOError as err:
+                # Error logging
+                print(f'Error in get_anime_with_genre(): could not open file {image_file_path}')
+                print(str(err))
+                if final_result['successful']: final_result['successful'] = False
+            except KeyError as err:
+                # Error logging
+                print(f'Error in get_anime_with_genre(): the selected anime was missing one of the following keys ["title", "synopsis", "image_url"]')
+                print(str(err))
+                if final_result['successful']: final_result['successful'] = False
+            
+    return final_result
